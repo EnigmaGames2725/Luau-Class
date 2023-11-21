fcada703a87f8d146581da2c7b56a54b2113d5ce
EnigmaGames2725 <99771570+EnigmaGames2725@users.noreply.github.com>
2023-11-20 20:13:43 -0500

    Re-wrote stack.lua
    Re-written.
    - Added __add metamethod
    - Added Stack:Peek()

--- src/stack.lua
+++ src/stack.lua
@@ -1,26 +1,44 @@
 local Stack = {}
 
 local Schema = {}
-local Metatable = {__index = Schema}
+local Metatable = {
+	__index = Schema,
+	__add = function(t, v)
+		for i = 1, #v do
+			t:Push(v:Pop())
+		end
+	end,
+}
 
 function Stack.new()
-	local _Stack = setmetatable({}, Metatable)
-	
-	return _Stack
+	return setmetatable({}, Metatable)
 end
 
-function Schema:Push(Value)
+function Schema:Push(Value: any)
 	self[#self+1] = Value
 end
 
-function Schema:Pop(): any
+function Schema:Pop()
 	if #self ~= 0 then
 		local Value = self[#self]
 		self[#self] = nil
+		
 		return Value
 	else
 		return nil
 	end
 end
+
+function Schema:Peek(Depth: number?)
+	if #self ~= 0 then
+		local Value = self[#self - ((Depth or 1) - 1)]
+
+		return Value
+	else
+		return nil
+	end
+end
+
+export type Stack = typeof(Stack.new(table.unpack({...})))
 
 return Stack
